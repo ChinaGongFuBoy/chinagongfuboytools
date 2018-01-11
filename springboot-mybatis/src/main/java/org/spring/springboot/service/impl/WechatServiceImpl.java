761e94b2c3817b324f5ca00ca66b4c6ac83129b7
Administrator <yigang_wu@126.com>
2018-01-11 15:18:36 +0800

    add

---
+++ springboot-mybatis/src/main/java/org/spring/springboot/service/impl/WechatServiceImpl.java
@@ -0,0 +1,11 @@
+package org.spring.springboot.service.impl;
+
+import org.spring.springboot.service.WechatService;
+
+/**
+ * 微信服务接口的实现类
+ * @author yigang.wu
+ * @date created in $time $date
+ */
+public class WechatServiceImpl implements WechatService{
+}
