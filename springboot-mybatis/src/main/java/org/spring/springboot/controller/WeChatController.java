08a42d3a3829cff015096799a19ae925169de79c
Administrator <yigang_wu@126.com>
2018-01-31 16:13:30 +0800

    add

--- springboot-mybatis/src/main/java/org/spring/springboot/controller/WeChatController.java
+++ springboot-mybatis/src/main/java/org/spring/springboot/controller/WeChatController.java
@@ -46,6 +46,7 @@ public class WeChatController {
         }
         out.close();
 
+
     }
 
     @RequestMapping(value = "/authorize", method = RequestMethod.GET)
--- springboot-mybatis/src/main/java/org/spring/springboot/service/impl/WechatServiceImpl.java
+++ springboot-mybatis/src/main/java/org/spring/springboot/service/impl/WechatServiceImpl.java
@@ -8,4 +8,5 @@ import org.spring.springboot.service.WechatService;
  * @date created in $time $date
  */
 public class WechatServiceImpl implements WechatService{
+
 }
